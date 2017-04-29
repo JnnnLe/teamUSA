901c7338f264479715ed7ca6234e4b6021d52d7c
Eli Cohen <elicohen2018@u.northwestern.edu>
2017-04-29 15:49:58 -0700

    code works now

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './app.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+render( <MuiThemeProvider><AppContainer><App/></AppContainer></MuiThemeProvider>, document.querySelector("#app"));
 
 if (module && module.hot) {
   module.hot.accept('./app.jsx', () => {
     const App = require('./app.jsx').default;
     render(
+      <MuiThemeProvider>
       <AppContainer>
         <App/>
-      </AppContainer>,
+      </AppContainer>
+      </MuiThemeProvider>,
       document.querySelector("#app")
     );
   });
